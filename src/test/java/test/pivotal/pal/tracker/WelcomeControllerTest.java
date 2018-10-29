b9609927216107046ac6f0314374d7866d4a43c6
pal_user <tjohn365@ford.com>
2018-10-29 15:52:07 -0400

    Deploy a Spring Boot App To Cloud Foundry Lesson

--- src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/WelcomeControllerTest.java
@@ -9,8 +9,8 @@ public class WelcomeControllerTest {
 
     @Test
     public void itSaysHello() throws Exception {
-        WelcomeController controller = new WelcomeController("A welcome message");
+        WelcomeController controller = new WelcomeController("Hello from test");
 
-        assertThat(controller.sayHello()).isEqualTo("A welcome message");
+        assertThat(controller.sayHello()).isEqualTo("Hello from test");
     }
 }
