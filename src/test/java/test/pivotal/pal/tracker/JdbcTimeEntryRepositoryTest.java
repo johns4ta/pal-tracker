8dac4824f4bf4bce8cebcb183c330ea98c18f508
pal_user <tjohn365@ford.com>
2018-10-31 13:44:24 -0400

    Added JDBC connection set up

--- src/test/java/test/pivotal/pal/tracker/JdbcTimeEntryRepositoryTest.java
+++ src/test/java/test/pivotal/pal/tracker/JdbcTimeEntryRepositoryTest.java
@@ -39,6 +39,7 @@ public class JdbcTimeEntryRepositoryTest {
         TimeEntry newTimeEntry = new TimeEntry(123, 321, LocalDate.parse("2017-01-09"), 8);
         TimeEntry entry = subject.create(newTimeEntry);
 
+        System.out.println("JdbcTemplate:"+jdbcTemplate+" Entry:"+entry+", subject:"+subject);
         Map<String, Object> foundEntry = jdbcTemplate.queryForMap("Select * from time_entries where id = ?", entry.getId());
 
         assertThat(foundEntry.get("id")).isEqualTo(entry.getId());
